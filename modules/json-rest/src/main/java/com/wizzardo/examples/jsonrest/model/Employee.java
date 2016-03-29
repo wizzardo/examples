ae4d9ee172f9098c86ce83eb5c6a5bd0e7aef71f
wizzardo <wizzardobox@gmail.com>
2016-03-29 16:51:10 +0300

    add simple pojo

---
+++ modules/json-rest/src/main/java/com/wizzardo/examples/jsonrest/model/Employee.java
@@ -0,0 +1,12 @@
+package com.wizzardo.examples.jsonrest.model;
+
+import java.util.Date;
+
+/**
+ * Created by wizzardo on 29.03.16.
+ */
+public class Employee {
+    int id;
+    String name;
+    Date createdDate;
+}
