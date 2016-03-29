0a969b1b84105620dd50b70dff29d66d68d948fd
wizzardo <wizzardobox@gmail.com>
2016-03-29 16:50:03 +0300

    create main class and add dependencies

---
+++ modules/json-rest/src/main/java/com/wizzardo/examples/jsonrest/App.java
@@ -0,0 +1,14 @@
+package com.wizzardo.examples.jsonrest;
+
+/**
+ * Created by wizzardo on 29.03.16.
+ */
+public class App {
+
+    public App() {
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
